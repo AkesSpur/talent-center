8fd7d3ab01c7ccb55c1bda8fd7ba51b45ac33510
AkesSpur <crystallord2005@gmail.com>
2026-04-07 20:17:47 +0300

    changed back to use the dynamic amount

--- app/Services/TBankService.php
+++ app/Services/TBankService.php
@@ -39,8 +39,7 @@ class TBankService
 
         $params = [
             'TerminalKey'     => $this->terminalKey,
-            // 'Amount'          => $amount,
-            'Amount'          => 1000,
+            'Amount'          => $amount,
             'OrderId'         => $orderId,
             'Description'     => 'Оргвзнос за участие в конкурсе «' . $contest->title . '»',
             'SuccessURL'      => route('payments.success', ['order' => $orderId]),
